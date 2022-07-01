ceb83c65e6a5363dd84241d5a3aee72e0ab8600c
Henrique Monteiro Martins <henrique.monteiro.martins@hotmail.com>
2022-07-01 09:51:09 -0300

    backup

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
 import { stripe } from '../../services/stripe';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './style.module.scss';
 
 interface SubscribeButtonProps{
@@ -9,12 +11,21 @@ interface SubscribeButtonProps{
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
   const session = useSession();
 
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn('github')
       return;
     }
 
+    try{
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripe = getStripeJs()
+
+      await (await stripe).redirectToCheckout({ sessionId: sessionId })
+    }catch (err){
+      alert(err.message)
+    }
     
   }
 
